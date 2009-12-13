a52dda63fb7bdbe0734b7caaa5bb9598d7483ffc
Larry Diehl <larry@laptoptheliquid.local>
2009-12-12 16:28:39 -0800

    First words of each line

---
+++ InteractWith.hs
@@ -0,0 +1,18 @@
+import System.Environment (getArgs)
+
+firstOfEachLine text = unwords firstWords
+  where firstWord line = head (words line)
+        firstWords = firstWord `map` (lines text)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        myFunction = firstOfEachLine
