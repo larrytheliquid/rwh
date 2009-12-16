55a460b6f8c321e524e0f4aa0d9c535548366d1f
Larry Diehl <larry@laptoptheliquid.local>
2009-12-15 18:14:32 -0800

    foldl' better because it can shortcircuit without building a thunk

--- Exercises.hs
+++ Exercises.hs
@@ -160,8 +160,9 @@ prop_myGroupBy_model :: [Int] -> Bool
 prop_myGroupBy_model xs = myGroupBy (==) xs == groupBy (==) xs
 
 myAny :: (a -> Bool) -> [a] -> Bool
-myAny f xs = foldr f' False xs
-  where f' x acc = f x || acc
+myAny f xs = foldl' f' False xs
+  where f' acc x = f x || acc
         
 prop_myAny_model :: [Int] -> Bool
 prop_myAny_model xs = myAny even xs == any even xs
+
